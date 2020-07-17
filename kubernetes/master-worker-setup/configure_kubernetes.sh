2a176db3c2374fe0184a2c257cf0fd8bf4195dc5
Moruf Ajibike <moruf@evari.insure>
2020-07-17 22:05:14 +0100

    Consolidate docker and kubernetes scripts

--- kubernetes/master-worker-setup/configure_kubernetes.sh
+++ kubernetes/master-worker-setup/configure_kubernetes.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce=5:19.03.12~3-0~ubuntu-bionic
+
+sudo apt-mark hold docker-ce
+
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
@@ -8,7 +21,7 @@ EOF
 
 sudo apt-get update
 
-sudo apt-get install -y kubelet=1.15.7-00 kubeadm=1.15.7-00 kubectl=1.15.7-00
+sudo apt-get install -y kubelet=1.17.8-00 kubeadm=1.17.8-00 kubectl=1.17.8-00
 
 sudo apt-mark hold kubelet kubeadm kubectl
 
