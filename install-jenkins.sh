7712f2e6866a12d6d4d408aab1a6737b4dad5abf
Moruf Ajibike <abiodun.ajibike1@yahoo.com>
2020-10-14 11:34:16 +0100

    Add jenkins installation script

---
+++ install-jenkins.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt install -y openjdk-8-jre
+
+wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
+
+java -jar jenkins.war --httpPort=80 --prefix=/dashboard
+
+# To display the password, enter
+# sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+# sudo cat /var/log/jenkins/jenkins.log
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+sudo apt-get update
+sudo apt-get install -y openjdk-8-jre jenkins
+
+sudo systemctl status jenkins
+
+sudo systemctl enable jenkins
