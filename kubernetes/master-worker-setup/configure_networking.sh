11b2f9959e6e5ceadf4a2c96496fcaf409b8bb9f
Moruf Ajibike <abiodun.ajibike1@yahoo.com>
2021-08-08 23:06:01 +0100

    Bug fix sysctl networking

--- kubernetes/master-worker-setup/configure_networking.sh
+++ kubernetes/master-worker-setup/configure_networking.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# To create /etc/systcl.conf, if not present
+sudo modprobe br_netfilter
+
 # On all three nodes, run the following:
 echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
 
